923a8b682c3d61defb8a00ac0c82379e8b00a7a6
Serena <serenafgreen@gmail.com>
2024-06-06 20:53:24 -0700

    adds welcome message + instructions

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const newQuoteBtn = document.getElementById("newQuoteBtn");
   const quoteElement = document.getElementById("quote");
   const authorElement = document.getElementById("author");
+  const sportElement = document.getElementById("sport");
+  const teamElement = document.getElementById("team");
 
   const fetchQuote = async () => {
     try {
@@ -9,7 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
       const data = await response.json();
 
       quoteElement.textContent = data.quote;
-      authorElement.textContent = `- ${data.author}`;
+      authorElement.innerHTML = `- ${data.author}, <span class="grey-text">${data.team}</span>`;
+      teamElement.textContent;
     } catch (error) {
       console.error("Error fetching quote:", error);
     }
